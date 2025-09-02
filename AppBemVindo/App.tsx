63b202144ede525637a6bf7b3da5ec01c789ba04
k-auaferreira <kaua.fs@outlook.com>
2025-09-01 21:11:08 -0300

    :sparkles: feat: added useState with alert in login screen

--- AppBemVindo/App.tsx
+++ AppBemVindo/App.tsx
@@ -1,41 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 //Componente principal do app
 const App: React.FC = () => {
-
   //Declarando os estados 'name' e 'greeting' com useState
   const [name, setName] = useState('');
   const [greeting, setGreeting] = useState('');
+  const handleButtonPress = (): void => {
+    Alert.alert('Ops!', 'Por favor, preencha o campo com seu nome.');
+  };
 
   // Função chamada quando o botão é precionado 
   const handlePress = () => {
-    //Poderiamos ter um alerta aqui 
-    if (greeting) { //Se já houver uma saudação, reseta os estatados
-      setName(''); //limpa o nome
-      setGreeting(''); //limpa a saudação 
-    } else { //Se não houver saudação, cria uma nova
-      setGreeting(`Olá, ${name}!`); //Cria uma nova saudação
+
+    // Poderiamos ter um alerta aqui
+    if (name === '') {
+      handleButtonPress();
+      return;
+    }
+
+    if (greeting) { // Se já houver uma saudação, reseta os estatados
+      setName(''); // Limpa o nome
+      setGreeting(''); // Limpa a saudação 
+    } else { // Se não houver saudação, cria uma nova
+      setGreeting(`Olá, ${name}!`); // Cria uma nova saudação
     }
   };
 
   return (
     //Criamos a view com o text e com a saudação 
-
     <View style={styles.container}>
-      {/*Aqui estamos fazendo o text */}
+
+      {/* Aqui estamos fazendo o text */}
       <Text style={styles.text}>
         {!greeting ? 'Qual é o seu nome?' : greeting}
       </Text>
-      {/*Aqui estamos fazendo o o textinput */}
+
+      {/* Aqui estamos fazendo o o textinput */}
       <TextInput
         style={styles.input}
-        value={name} //Valor do TextInput é o estado 'name'
-        onChangeText={setName} //Atualiza o estado 'name' quando o texto muda
-        placeholder="Digite seu nome" //Texto placeholder
+        value={name} // Valor do TextInput é o estado 'name'
+        onChangeText={setName} // Atualiza o estado 'name' quando o texto muda
+        placeholder="Digite seu nome" // Texto placeholder
       />
-      {/*Aqui estamos fazendo o o textinput */}
+      {/* Aqui estamos fazendo o o textinput */}
       <TouchableOpacity style={styles.button} onPress={handlePress}>
         <Text style={styles.buttonText}>
           {greeting ? 'Limpar' : 'Enviar'}
@@ -49,15 +58,31 @@ const App: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //Faz o conteiner ocupar toda a alteura da tela 
+    backgroundColor: '#fff', // alinha os itens verticalmente no centro 
+    alignItems: 'center', // alinha os itens horizontalmente no centro
+    justifyContent: 'center', // alinha os itens no centro
   },
   text: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    border
-  }
+    fontSize: 20,
+    margin: 10,
+  },
+  input: {
+    width: '80%', // Largura do campo de entrada, ocupando 80% da tela
+    borderBottomWidth: 1, // Adiciona uma linha na parte inferior do campo de entrada
+    padding: 5,  // Preenchimento dentro do campo de entrada
+    marginBottom: 20, // Adiciona espaço abaixo do campo de entrada
+    textAlign: 'center', // Alinha o texto digitado no centro
+  },
+  button: {
+    backgroundColor: '#ED145B',
+    padding: 12,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
 });
+
+export default App;
