013decbb98b8f7e4fa3fd0e26936b6e8bc0be9c8
k-auaferreira <kaua.fs@outlook.com>
2025-09-08 20:05:57 -0300

    feat: add random number generation (1-10) on button click

--- AppButton/App.tsx
+++ AppButton/App.tsx
@@ -1,40 +1,43 @@
 // AppButton com Toast
-import React, {useState} from "react"; //ESTADO DO COMPONENTE
-import {View, Text, Button, StyleSheet} from "react-native";
+import React, { useState } from "react"; //ESTADO DO COMPONENTE
+import { View, Text, Button, StyleSheet } from "react-native";
 import Toast from "react-native-toast-message";
 
 export default function App() {
-// Estado para controlar a mensagem de texto a ser exibida 
-  const [message, setMessage] = useState ("Precione o botão");
+  // Estado para controlar a mensagem de texto a ser exibida 
+  const [message, setMessage] = useState("Nenhum número sorteado");
 
   //Função chamada quando o botão é pressionado
   const handleButtonClick = () => {
 
+    const numeroSorteado = Math.floor(Math.random() * 10) + 1;
+
     // Atualiza a mensagem de texto
-    setMessage("Botão Pressionado!");
+    setMessage(`Número sorteado: ${numeroSorteado}`);
 
     //Exiba o toast 
-    showToast();
+    // showToast();
   };
 
- return (
+  return (
     <View style={styles.container}>
+      <Text style={styles.text}>Sortear um número</Text>
+      <Button title="Sortear" onPress={handleButtonClick} />
       <Text style={styles.text}>{message}</Text>
-      <Button title="Pressione-me" onPress={handleButtonClick} />
       <Toast /> {/* Componente necessário para exibir os toasts */}
     </View>
   );
 }
 
- // Função para exibir o toast
-  const showToast = () => {
-    Toast.show({
-      type: 'success', //Tipo do toast que pode ser 'success', 'error', ou 'info', etc
-      text1: 'Ação realizada com sucesso!', //texto principal do toast
-      text2: 'Você pressionou o botão! 👋', 
-      visibilityTime: 2000, //Duração em milissegundos que o toast ficará visível
-    });
-  };
+// Função para exibir o toast
+// const showToast = () => {
+//   Toast.show({
+//     type: 'success', //Tipo do toast que pode ser 'success', 'error', ou 'info', etc
+//     text1: 'Ação realizada com sucesso!', //texto principal do toast
+//     text2: 'Você pressionou o botão! 👋', 
+//     visibilityTime: 2000, //Duração em milissegundos que o toast ficará visível
+//   });
+// };
 
 //Definição dos estilos
 const styles = StyleSheet.create({
@@ -45,6 +48,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5f5",
   },
   text: {
-    fontSize: 20,  
-  }, 
+    fontSize: 20,
+    marginVertical: 10,
+  },
 });
