972111c1526a5da4dab6feab52cab25a2c74a21c
k-auaferreira <kaua.fs@outlook.com>
2025-09-08 21:04:17 -0300

    :sparkles: feat: show concatenated texts in custom alert

--- AppAlert/App.tsx
+++ AppAlert/App.tsx
@@ -1,16 +1,20 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
 // Exemplo de uso de Alert com personalização 
 function App() {
-  const showCustomAlert = () => {
+  const [text1, setText1] = useState("Empresa bacana");
+  const [text2, setText2] = useState("Kauã Ferreira");
+
+  // Função chamada quando o botão é pressionado
+  const handleButtonClick = () => {
+    const mensagem = `${text1} - ${text2}`;
     Alert.alert(
-      'Alerta Personalizado',
-      'Este é um alerta com botões personalizados.',
+      'Alert',
+      mensagem,
       [
-        {text: 'Ação 1', onPress: () => console.log('Ação 1 Pressionada')},
-        {text: 'Ação 2', onPress: () => console.log('Ação 2 Pressionada')},
-        {text: 'Fechando!', style: 'cancel', onPress: () => console.log('Feshow!')},
+        { text: "OK", style: "cancel" }
       ],
       { cancelable: true }
     );
@@ -18,31 +22,38 @@ function App() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={ styles.button} onPress={showCustomAlert}>
-        <Text style={styles.buttonText}>Aperte! </Text>
+      <Text style={styles.text}>{text1}</Text>
+      <Text style={styles.text}>{text2}</Text>
+      <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
+        <Text style={styles.buttonText}>COMBINAR TEXTOS</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-//Definiação estilos
+// Definição estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   buttonText: {
-    color: '#ffffffff',
-     fontSize: 16,
-     fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },  
   button: {
     backgroundColor: '#ED145B',
     padding: 10,
     borderRadius: 5,
+    marginVertical: 5,
+  },
+  text: {
+    color: 'white',
+    marginVertical: 5,
   },
 });
 
